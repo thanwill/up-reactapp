b5ee3a246ba4e706d38ff90b92e3d60447f3a019
Jonathan Pereira <jonathan14willian@icloud.com>
2023-03-21 15:52:09 -0300

    *ortográfia

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -8,7 +8,7 @@ function Inicio() {
         <div className="col-10 offset-1 pt-5">
         <h1 className="text-left pb-3">Vamos jogar?</h1>
         <p className="text-justify pb-3 text-left">
-        Enquanto aguardamos a contrução das páginas e o desenrolar das aulas, separei um jogo clássico disponível na documentação do React ilustrando algumas das funcionalidades disponibilizadas pela ferramenta. 
+        Enquanto aguardamos a construção das páginas e o desenrolar das aulas, separei um jogo clássico disponível na documentação do React ilustrando algumas das funcionalidades disponibilizadas pela ferramenta. 
         </p>
         </div>
       </div>
