1c191daa1c289e10399bc8cc48d7b7868636a29b
John Doe <jonathan14willian@icloud.com>
2023-03-27 21:46:35 -0300

    Adição de comentários

---
+++ src/ConfigRoutes.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Inicio from "./pages/inicio";
+import Catalogo from "./pages/catalogo";
+import Erro from "./pages/erro.js";
+import Login from "./pages/login";
+import Planos from "./pages/planos";
+
+export default function ConfigRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Catalogo />} />
+      <Route path="/jogos" element={<Inicio />} />
+      <Route path="/logar" element={<Login />} />
+      <Route path="/planos" element={<Planos />} />
+      <Route path="*" element={<Erro />} />
+    </Routes>
+  );
+}
